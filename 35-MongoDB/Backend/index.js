d2a48f6968d03ea95b25be6b6ea709d1036e0c11
lekshmimb <lekshmi.mb@iiitmk.ac.in>
2026-02-03 20:55:29 +0530

    KBA courses MongoDB

---
+++ 35-MongoDB/Backend/index.js
@@ -0,0 +1,44 @@
+import express, { json } from "express"
+import dotenv from "dotenv"
+import { router } from "./Route/trialrout.js"
+import { admin } from "./Route/adminroute.js"
+import { authenticate } from "./Middleware/auth.js"
+import admincheck from "./Middleware/admin.js"
+import user from "./Route/courseroute.js"
+import mongoose from "mongoose"
+
+
+
+dotenv.config()
+
+const app = express()
+app.use(json())
+// app.use("/user", router)
+app.use("/user", router)
+app.use("/admin", authenticate, admincheck, admin)
+app.use("/", user)
+
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the website")
+})
+
+app.get("/homepage", (req, res) => {
+    res.send("welcome to homepage")
+    console.log("welcome to homepage")
+})
+
+const mongodbURI = process.env.MONGODB_URI || "mongodb://localhost:27017/KBA-COURSE"
+mongoose.connect(mongodbURI).then(() => {
+    console.log("MongoDB connected")
+}).catch((err) => {
+    console.log("MongoDB connection error:", err)
+})
+
+
+app.listen(process.env.port, () => {
+    console.log(`SERVER is running at ${process.env.port}`);
+
+
+})
