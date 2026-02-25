7922b521c259fc2a18a5f4c9d56eb91e834f967b
nikhilvc1990 <nikhil.chandran@iiitmk.ac.in>
2026-02-25 11:48:27 +0530

    adding kbacourses

---
+++ 42-React/kbacourses/ui/src/App.jsx
@@ -0,0 +1,22 @@
+
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import TopCourses from './components/TopCourses'
+import CourseGrid from './components/CourseGrid'
+import AllCoursesButton from './components/AllCoursesButton'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <TopCourses />
+      <CourseGrid />
+      <AllCoursesButton />
+    </div>
+  )
+}
+
+export default App
